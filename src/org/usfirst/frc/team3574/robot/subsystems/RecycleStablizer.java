1f16e61a28dd962852f40ca4fa0d6972c30e3c27
first3574 <first3574@gmail.com>
2015-04-11 10:57:04 -0700

    Added Subsystem for stabizer and two servos for it

---
+++ src/org/usfirst/frc/team3574/robot/subsystems/RecycleStablizer.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team3574.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class RecycleStablizer extends Subsystem {
+	Servo stablizerServo;
+	Servo lockingServo;
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
